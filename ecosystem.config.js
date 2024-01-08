b375c16e6664ffb9fc94c6668ba89d05f064f7b2
shakibesaib <shakibesaib@gmail.com>
2024-01-08 15:04:52 +0600

    adding ecosystem config file

---
+++ ecosystem.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  apps: [
+    {
+      name: "nextjs-app", // Choose a name that makes sense for your production environment
+      script: "npm",
+      args: "start",
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+  deploy: {
+    local: {
+      user: "shakibe",
+      host: "localhost",
+      ref: "origin/main",
+      repo: "https://github.com/ShakibeSaib/my-next-app.git",
+      path: "/path/to/deploy-directory",
+      "pre-deploy-local": "npm install",
+      "post-deploy": "npm run build && pm2 reload ecosystem.config.js --env production",
+    },
+  },
+};
+
