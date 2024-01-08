777a4c6ef19a4d3de22c8398a854a274e9a68750
shakibesaib <shakibesaib@gmail.com>
2024-01-08 11:52:43 +0600

    Initial Commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+// pages/index.js
+
+// pages/index.js
+
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Hello Shakibe</h1>
+    </div>
+  );
+};
+
+export default Home;
+
